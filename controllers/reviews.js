79590e256b35a32884025520adae6a8323f757a4
Mimi <trish21215@yahoo.com>
2023-03-28 13:14:40 -0400

    Add more pseudocode and update the update function and add edit function

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,7 +2,8 @@ const BathroomModel = require('../models/bathroom');
 
 module.exports = {
     create,
-    update
+    update,
+    edit
 }
 
 function create(req, res) {
@@ -27,19 +28,45 @@ function create(req, res) {
     })
 }
 
-function update(req, res, next) {
-    console.log(req.user, '<-req.user')
-    console.log(req.params, '<-req.params')
-    // calling on the review with a specific id to update
-BathroomModel.findOneAndUpdate({'reviews._id': 
-// calling on the user that made the review with a specific id
-req.params.id, 'reviews.userId': req.user._id})
-// updating the review document with new information
-.then(function(updateDoc) {
-// {new:true} returns the updated document
-req.body, {new:true}
- res.redirect(`/bathrooms/${updateDoc._id}`);
-}).catch(function(err) {
-    return next(err);
-})
+// function update(req, res, next) {
+//     console.log(req.user, '<-req.user')
+//     console.log(req.params, '<-req.params')
+//     // calling on the review with a specific id to update
+// BathroomModel.findOneAndUpdate({'reviews._id': 
+// // calling on the user that made the review with a specific id
+// req.params.id, 'reviews.userId': req.user._id})
+// // updating the review document with new information
+// .then(function(updateDoc) {
+// // {new:true} returns the updated document
+// req.body, {new:true}
+//  res.redirect(`/bathrooms/${updateDoc._id}`);
+// }).catch(function(err) {
+//     return next(err);
+// })
+// }
+
+
+function update(req, res) {
+    // using the review id to pluck it from the db
+    BathroomModel.findOne({'reviews._id':req.params.
+    id}).then(function(updateDoc) {
+        // accessing the review by it's id and accessing the text propety
+        updateDoc.reviews.id(req.params.id).text = req.body.text;
+        // making sure the user id property matches the id of the logged in user
+        if (!updateDoc.reviews.id(req.params.id).userId.equals(req.user._id))
+        updateDoc.save().then(() => {
+            return res.redirect(`/review/${BathroomModel.id}`);
+        }).catch(function(err) {
+            console.log(err)
+        })
+    })
+}
+
+
+function edit(req, res) {
+    // retrieving the review within the bathroom model
+    BathroomModel.findOne({'reviews._id': req.params.
+    id}).then(function(editBr) {
+        res.render('reviews/edit', {review:editBr.reviews.id(req.params.id)});
+    })
 }
